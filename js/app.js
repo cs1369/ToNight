09e61212f1206f826a5ccb1be67ff6e927a5a6b8
cs1369 <suarezgabriel420@gmail.com>
2022-06-19 18:46:44 -0300

    agrego fondo responsive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const btnMenu= document.querySelector(".mobile__menu-icon");
 const menuListas=document.querySelectorAll(".mobile__menu-items");
+const menuIcono = document.querySelectorAll(".mobile__menu-line");
 const menu= document.querySelector(".mobile__menu");
 btnMenu.addEventListener("click",()=>{
     menu.classList.toggle("mobile__menu-visibility");
+    menuIcono[0].classList.toggle("mobile__menu-line-animation1");
+    menuIcono[1].classList.toggle("mobile__menu-line-animation2");
+    menuIcono[2].classList.toggle("mobile__menu-line-animation3");
     for(let lista of menuListas){
         lista.classList.toggle("mobile__menu-items-animation");
     }
