4c355ff3395d29a06ad43e7395d0dc0b15ac0cd9
cs1369 <suarezgabriel420@gmail.com>
2022-06-22 03:32:28 -0300

    inicio login-mobile

--- js/app.js
+++ js/app.js
@@ -11,3 +11,12 @@ btnMenu.addEventListener("click",()=>{
         lista.classList.toggle("mobile__menu-items-animation");
     }
 });
+menu.addEventListener("click",()=>{
+    menu.classList.toggle("mobile__menu-visibility");
+    menuIcono[0].classList.toggle("mobile__menu-line-animation1");
+    menuIcono[1].classList.toggle("mobile__menu-line-animation2");
+    menuIcono[2].classList.toggle("mobile__menu-line-animation3");
+    for(let lista of menuListas){
+        lista.classList.toggle("mobile__menu-items-animation");
+    }
+});
